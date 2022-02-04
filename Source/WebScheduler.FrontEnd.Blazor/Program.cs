76e03e74ce4e77409231420e1b79569756865da7
Elan Hasson <234704+ElanHasson@users.noreply.github.com>
2022-02-04 03:23:52 -0500

    Implement ODIC for account.nullreference.io

--- Source/WebScheduler.FrontEnd.Blazor/Program.cs
+++ Source/WebScheduler.FrontEnd.Blazor/Program.cs
@@ -1,3 +1,4 @@
+using ITfoxtec.Identity.BlazorWebAssembly.OpenidConnect;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WebScheduler.FrontEnd.Blazor;
@@ -6,13 +7,34 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddOidcAuthentication(options =>
+builder.Services.AddAuthorizationCore(c => { });
+
+builder.Services.AddHttpClient("frontend", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+       .AddHttpMessageHandler<AccessTokenMessageHandler>();
+
+builder.Services.AddTransient(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("frontend"));
+
+builder.Services.AddOpenidConnectPkce(settings =>
 {
-    // Configure your authentication provider options here.
-    // For more information, see https://aka.ms/blazor-standalone-auth
-    builder.Configuration.Bind("Local", options.ProviderOptions);
+    builder.Configuration.Bind("IdentitySettings", settings);
+    
+});
+
+
+builder.Services.AddHttpClient("web-scheduler-api", client => client.BaseAddress = new Uri(builder.Configuration.GetValue<string>("WebScheduler:BaseUri")))
+     .AddHttpMessageHandler(sp =>
+     {
+         var handler = sp.GetRequiredService<AccessTokenMessageHandler>();
+         builder.Configuration.Bind("IdentitySettings", handler);
+         return handler;
+     });
+
+builder.Services.AddTransient(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorWebAssemblyOidcSample.API"));
+
+builder.Services.AddOpenidConnectPkce(settings =>
+{
+    builder.Configuration.Bind("IdentitySettings", settings);
 });
 
 await builder.Build().RunAsync();
