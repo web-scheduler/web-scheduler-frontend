2f245f428c1ce90fd79d9407c2d2b5f0e06a2902
Elan Hasson <234704+ElanHasson@users.noreply.github.com>
2022-02-19 12:19:42 -0500

    Add PageOptions and PageResults<ScheduledTask> to JsonSerializeContext

--- Source/WebScheduler.FrontEnd.Blazor/CustomJsonSerializerContext.cs
+++ Source/WebScheduler.FrontEnd.Blazor/CustomJsonSerializerContext.cs
@@ -4,7 +4,10 @@ using WebScheduler.Api.Models.ViewModels;
 
 [JsonSerializable(typeof(HttpMethod))]
 [JsonSerializable(typeof(SaveScheduledTask))]
+[JsonSerializable(typeof(SaveScheduledTask[]))]
 [JsonSerializable(typeof(ScheduledTask))]
+[JsonSerializable(typeof(PageResults<ScheduledTask>))]
+[JsonSerializable(typeof(PageOptions))]
 internal partial class CustomJsonSerializerContext : JsonSerializerContext
 {
 }
